f361e4765ae51c568f0d295b43ea0e861e39bb44
Linda Hrdinová <hrlin@seznam.cz>
2023-10-24 15:41:06 +0200

    zpracování cvičení na doma

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log('funguju!');
+
+const updateSkill = (level, skill) => {
+  const valueElement = document.querySelector(`#${skill} .skill__value`);
+  const progressElement = document.querySelector(`#${skill} .skill__progress`);
+  valueElement.textContent = `${level} / 100`;
+  progressElement.style.width = `${level}%`;
+};
+
+const html = Number(prompt('Jaká je vaše úroveň HTML? (0 až 100)'));
+const css = Number(prompt('Jaká je vaše úroveň CSS? (0 až 100)'));
+const js = Number(prompt('Jaká je vaše úroveň JavaScriptu? (0 až 100)'));
+
+updateSkill(html, 'skill1');
+updateSkill(css, 'skill2');
+updateSkill(js, 'skill3');
